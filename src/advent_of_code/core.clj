a03db1afa226a369535ed53912c8f5d73de14e3b
p4v4n <pavan.mantha99@gmail.com>
2021-12-03 09:28:16 +0530

    Add day 1

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -1,7 +1,31 @@
 (ns advent-of-code.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+;;--- Day 1: Sonar Sweep ---
+
+(defn count-seq-increase [seq]
+  (->> (map #(> %1 %2) (drop 1 seq) (drop-last 1 seq))
+       (filter identity)
+       count))
+
+(defn file-as-seq [file-path]
+  (-> (slurp file-path)
+      (str/split  #"\n")))
+
+(defn aoc1 []
+  (->> (file-as-seq "resources/input-1.txt")
+       (map read-string)
+       count-seq-increase))
+
+(defn count-seq-increase2 [seq]
+  (->> (map + seq (drop 1 seq) (drop 2 seq))
+       count-seq-increase))
+
+(defn aoc2 []
+  (->> (file-as-seq "resources/input-1.txt")
+       (map read-string)
+       count-seq-increase2))
+
+;;(aoc1)
+;;(aoc2)
