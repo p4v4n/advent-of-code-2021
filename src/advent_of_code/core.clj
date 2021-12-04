24bad2c95359e6921438100d504a1b1375cabab7
p4v4n <pavan.mantha99@gmail.com>
2021-12-04 10:26:34 +0530

    Add day2

--- src/advent_of_code/core.clj
+++ src/advent_of_code/core.clj
@@ -27,7 +27,36 @@
        (map read-string)
        count-seq-increase2))
 
-;;--------------------------------------------
+;;--- Day 2: Dive! ---
+
+(defn course-as-coordinate [course]
+  (let [[dirn dist] (str/split course #"\s+")
+        d (read-string dist)]
+    (condp = dirn
+      "up" [0 (- d)]
+      "down" [0 d]
+      "forward" [d 0])))
+
+(defn add-coordinates [[x1 y1] [x2 y2]]
+  [(+ x1 x2) (+ y1 y2)])
+
+(defn aoc3 []
+  (->> (file-as-seq "resources/input-2.txt")
+       (map course-as-coordinate)
+       (reduce add-coordinates [0 0])
+       (reduce *)))
+
+(defn add-coordinates-with-aim [[x1 y1 a] [x2 y2]]
+  [(+ x1 x2) (+ y1 (* x2 a)) (if (= x2 0) (+ a y2) a)])
+
+(defn aoc4 []
+  (->> (file-as-seq "resources/input-2.txt")
+       (map course-as-coordinate)
+       (reduce add-coordinates-with-aim [0 0 0])
+       drop-last
+       (reduce *)))
+
+;;------------------------------------------
 
 (defn all-answers []
   (->> (keys (ns-publics 'advent-of-code.core))
